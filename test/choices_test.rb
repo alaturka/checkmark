24d5d43eb2b9c70d379561c23b8533feda30bdaa
Recai Oktaş <roktas@gmail.com>
2022-06-19 14:42:58 +0300

    .

--- test/choices_test.rb
+++ test/choices_test.rb
@@ -5,23 +5,24 @@ require_relative './test_helper'
 module Checkmark
   class ChoicesTest < Minitest::Test
     TESTDATA = { A: 'first', B: 'second', C: 'third', D: 'fourth', E: 'fifth' }.freeze
+    Klass = Choices
 
     def test_construction_implicit
-      choices = Choices[**TESTDATA]
+      choices = Klass[**TESTDATA]
 
       assert_equal(:A, choices.correct)
       assert_equal('first', choices.correct_choice)
     end
 
     def test_construction_explicit
-      choices = Choices[:C, **TESTDATA]
+      choices = Klass[:C, **TESTDATA]
 
       assert_equal(:C, choices.correct)
       assert_equal('third', choices.correct_choice)
     end
 
     def test_like_hash
-      choices = Choices[**TESTDATA]
+      choices = Klass[**TESTDATA]
 
       assert_equal(TESTDATA.size, choices.size)
 
@@ -30,7 +31,7 @@ module Checkmark
     end
 
     def test_shuffle_implicit
-      choices = Choices[**TESTDATA]
+      choices = Klass[**TESTDATA]
 
       correct_choice_initial = choices.correct_choice
 
@@ -41,7 +42,7 @@ module Checkmark
     end
 
     def test_shuffle_explicit
-      choices = Choices[:E, **TESTDATA]
+      choices = Klass[:E, **TESTDATA]
 
       correct_choice_initial = choices.correct_choice
 
@@ -52,21 +53,7 @@ module Checkmark
     end
   end
 
-  class CompactChoicesTest < Minitest::Test
-    TESTDATA = { A: 'first', B: 'second', C: 'third', D: 'fourth', E: 'fifth' }.freeze
-
-    def test_construction_implicit
-      choices = CompactChoices[**TESTDATA]
-
-      assert_equal(:A, choices.correct)
-      assert_equal('first', choices.correct_choice)
-    end
-
-    def test_construction_explicit
-      choices = CompactChoices[:C, **TESTDATA]
-
-      assert_equal(:C, choices.correct)
-      assert_equal('third', choices.correct_choice)
-    end
+  class CompactChoicesTest < ChoicesTest
+    Klass = CompactChoices
   end
 end
