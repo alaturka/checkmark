8b450be14c9ae0926c5476c2378bab9797f9533f
Recai Oktaş <roktas@gmail.com>
2022-06-20 16:16:29 +0300

    .

--- lib/checkmark.rb
+++ lib/checkmark.rb
@@ -10,5 +10,6 @@ require_relative 'checkmark/item'
 require_relative 'checkmark/quiz'
 
 require_relative 'checkmark/loader'
+require_relative 'checkmark/plugin'
 require_relative 'checkmark/reader'
 require_relative 'checkmark/writer'
