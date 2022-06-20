196b7329de986815b0a4d11d1abdedf99993f252
Recai Oktaş <roktas@gmail.com>
2022-06-20 19:38:20 +0300

    .

--- lib/checkmark/cli.rb
+++ lib/checkmark/cli.rb
@@ -1,21 +1,63 @@
 # frozen_string_literal: true
 
-require 'checkmark'
-
-require 'dry/cli'
+require 'optparse'
 
 module Checkmark
   module CLI
-    module Commands
-      extend Dry::CLI::Registry
+    def self.call(*argv, **kwargs)
+      options = {}.merge(kwargs)
+      args setup(argv, options), argv
 
-      Dir[File.join(__dir__, 'cli/*.rb')].each { |command| require command }
+      Checkmark.(content, options, from: from, to: to)
+    rescue OptionParser::InvalidOption, Error => e
+      abort(e.message)
+    end
 
-      register 'compile', Compile, aliases: ['c']
-      register 'export',  Export,  aliases: ['e']
-      register 'import',  Import,  aliases: ['i']
-      register 'lint',    Lint,    aliases: ['l']
-      register 'version', Version, aliases: ['v', '-v', '--version']
+    class << self
+      private
+
+      # rubocop:disable Metrics/MethodLength
+      # codebeat:disable[LOC]
+      def setup(argv, _options)
+        Signal.trap('INT') { Kernel.abort '' }
+
+        OptionParser.new do |option|
+          program_name = option.program_name
+          option.banner = <<~BANNER
+            Usage: #{program_name} [options...] <FILE>
+
+            See #{program_name}(1) manual page for detailed help.
+
+            Options:
+
+          BANNER
+
+          option.on_tail('-h', '--help', 'Show this message') do
+            abort option.help
+          end
+
+          option.on_tail('-v', '--version', 'Show version') do
+            warn VERSION
+            exit
+          end
+        end.tap { |parser| parser.parse!(argv) } # rubocop:disable Style/MultilineBlockChain
+      end
+      # codebeat:enable[LOC]
+      # rubocop:enable Metrics/MethodLength
+
+      def args(parser, argv)
+        if argv.empty?
+          warn parser.help
+          warn ''
+          abort 'Error: TODO'
+        end
+
+        return if argv.size <= 1
+
+        warn parser.help
+        warn ''
+        abort 'Error: Too many arguments.'
+      end
     end
   end
 end
