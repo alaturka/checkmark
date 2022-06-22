b31385b15117b70d71f63a90a937f8a8589e9111
Recai Oktaş <roktas@gmail.com>
2022-06-22 21:57:38 +0300

    .

--- lib/checkmark.rb
+++ lib/checkmark.rb
@@ -14,32 +14,33 @@ require_relative 'checkmark/write'
 class Checkmark
   attr_reader :source, :read, :process, :settings, :bank
 
-  def initialize(source, read:, process: [], settings: {})
-    @source   = source
-    @read     = read
-    @process  = process
-    @settings = settings
+  def initialize(source, reader:, processors: [])
+    @source     = source
+    @reader     = reader
+    @processors = processors
 
-    @bank     = load
+    @bank       = load
   end
 
-  def call(write:, emit: nil)
-    Write.handle(write) { |writer| writer.(Emit.handle(emit) { |emitter| emitter.(bank) }) }
+  def call(writer:, emitter: nil)
+    writer.(emitter.(bank))
   end
 
   private
 
   def load
-    Read.handle(read) { |reader| reader.(source) } # FIXME: processors
+    reader.(source) # FIXME: processors
   end
 
   class << self
-    def call(infile, outfile, emit: nil, process: [], settings: {})
-      read, write = Read.handler_name_for!(infile), Write.handler_name_for!(outfile)
+    def call(infile, outfile, emit: nil, processes: [], settings: {})
+      reader     = Read.handler_for_type(infile, settings.for(:read))
+      writer     = Write.handler_for_filetype!(outfile, settings.for(:write))
+      emitter    = Emit.handler(emit, settings.for(:emit))
+      processors = processes.map { Process.handler(_1, settings.for(:process)) }
 
-      new(Content.(infile), read: read, process: process, settings: settings).tap do |instance|
-        result = instance.(write: write, emit: emit)
-        File.write(outfile, result)
+      new(Content.(infile), reader: reader, processors: processors).tap do |instance|
+        File.write(outfile, instance.(writer: writer, emitter: emitter))
       end
     end
 
