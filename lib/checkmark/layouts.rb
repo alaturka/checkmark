d3ced891c8ddb1abfbd9359640301fc2ef59c261
Recai Oktaş <roktas@gmail.com>
2022-07-01 15:52:52 +0300

    .

--- lib/checkmark/layouts.rb
+++ lib/checkmark/layouts.rb
@@ -1,41 +1,5 @@
 # frozen_string_literal: true
 
-module Checkmark
-  class Template
-    attr_reader :type, :layout, :dir
-
-    def initialize(type, layout = "default")
-      @type   = type
-      @layout = layout
-      @dir    = File.join(__dir__, "layouts", type, layout)
-    end
-
-    def [](name)
-      File.join(dir, "#{name}.erb").tap { raise Error, "No template found in #{self}: #{name}" unless File.exist?(_1) }
-    end
-
-    def exist?(name = nil)
-      name ? File.exist?(self[name]) : Dir.exist?(dir)
-    end
-
-    def to_s
-      "#{type}/#{layout}"
-    end
-
-    def to_sym
-      to_s.to_sym
-    end
-  end
-
-  def self.template(...)
-    Template.new(...)
-  end
-
-  def self.template!(...)
-    template(...).tap { raise "Missing template: #{_1}" unless _1.exist? }
-  end
-
-  def self.template?(type, layout = "default", name = nil)
-    template(type, layout).exist?(name)
-  end
-end
+require_relative "layouts/html"
+require_relative "layouts/sil"
+require_relative "layouts/tex"
