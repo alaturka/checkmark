b5b617087a6f911914aa22133cdbac055c99e158
Recai Oktaş <roktas@gmail.com>
2022-07-01 12:40:39 +0300

    .

--- lib/checkmark/layouts.rb
+++ lib/checkmark/layouts.rb
@@ -1,11 +1,30 @@
 # frozen_string_literal: true
 
 module Checkmark
-  def self.layout(type, variant = "default")
-    File.join(__dir__, "layouts", type, "#{variant}.erb")
-  end
+  class Layout
+    attr_reader :type, :variant
 
-  def self.layout?(...)
-    File.exist?(layout(...))
+    def initialize(type, variant)
+      @type    = type
+      @variant = variant
+
+      @dir     = File.join(__dir__, "layouts", type, variant)
+    end
+
+    def template(name = nil)
+      name ? File.join(@dir, "#{name}.erb") : @dir
+    end
+
+    def template?(...)
+      File.exist?(templat(...))
+    end
+
+    def to_s
+      "#{type}/#{variant}"
+    end
+
+    def to_sym
+      to_s.to_sym
+    end
   end
 end
