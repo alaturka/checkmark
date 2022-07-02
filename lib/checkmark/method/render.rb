8058a3b0b6c3037ae7e421a5af9ba17709d6bc83
Recai Oktaş <roktas@gmail.com>
2022-07-02 19:33:46 +0300

    .

--- lib/checkmark/method/render.rb
+++ lib/checkmark/method/render.rb
@@ -4,6 +4,7 @@ module Checkmark
   class Method
     class Render < self
       require_relative "render/html"
+      require_relative "render/sil"
       require_relative "render/tex"
     end
   end
---
+++ lib/checkmark/method/render/sil.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Checkmark
+  class Method
+    class Render
+      class Sil < self
+      end
+    end
+  end
+end
