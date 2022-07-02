291a188fa37be7756e8a6e0639b7e74e4693c796
Recai Oktaş <roktas@gmail.com>
2022-07-02 19:23:05 +0300

    .

--- lib/checkmark/support/command.rb
+++ lib/checkmark/support/command.rb
@@ -17,8 +17,16 @@ module Checkmark
         !ok?
       end
 
+      def timeout?
+        Signal.signame(status&.termsig || 0) == 'XCPU'
+      end
+
       def canceled?
         canceled
+      end
+
+      def segfaulted?
+        Signal.signame(status&.termsig || 0) == 'SEGV'
       end
 
       def command
@@ -41,6 +49,19 @@ module Checkmark
       out, err, status = Open3.capture3(*argv, **kwargs)
 
       Result.new(out: out, err: err, argv: argv, status: status, canceled: canceled)
+    end
+
+    # rubocop:disable Layout
+    RLIMIT_SAFE = {                             # See https://stackoverflow.com/questions/38419570/setrlimit-in-ruby
+      rlimit_cpu:   [15,                   16], # 15 seconds, +1 for hard limit to signal XCPU before reaching hard limit
+      rlimit_as:    [536_870_912, 536_870_912], # 512 MB, half of the memory of a micro instance
+      rlimit_data:  [536_870_912, 536_870_912], # 512 MB, half of the memory of a micro instance
+      rlimit_core:  0                           # no core dump
+    }.freeze
+    # rubocop:enable Layout
+
+    def self.run_limited(*argv, **kwargs)
+      run(*argv, **RLIMIT_SAFE.merge(kwargs))
     end
   end
 end
